78d444b207d36d9950352def8468fd7f66f3a40a
Calen Pennington <cale@edx.org>
2015-03-17 11:30:27 -0400

    Add the ability to generate confidence intervals for percentile values

--- setup.py
+++ setup.py
@@ -54,5 +54,8 @@ setup(
             'locust = locust.main:main',
         ]
     },
+    extras_require={
+        'scipy': ['numpy', 'scipy', 'scikits.bootstrap'],
+    },
     test_suite='unittest2.collector',
 )
